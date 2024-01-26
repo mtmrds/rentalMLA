f6d3a7062b50db223bab5bbe8e62fb02b0e09319
takahiro <iketaka.2116810@gmail.com>
2024-01-26 10:43:46 +0900

    aaa

--- src/main/java/jp/ken/rental/controller/LoginFinController.java
+++ src/main/java/jp/ken/rental/controller/LoginFinController.java
@@ -1,5 +1,25 @@
 package jp.ken.rental.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+@RequestMapping("finish")
+@SessionAttributes({"loginModel", "membersModel"})
 public class LoginFinController {
 
+	@RequestMapping(method = RequestMethod.POST)
+	public String toFinish(Model model) {
+		model.addAttribute("message", "登録が完了しました");
+		return "registrationComplete";
+	}
+	
+	public String toLogout(Model model, SessionStatus status) {
+		status.setComplete();
+		return "redirect:/logintop";
+	}
 }
