aa0b1adaff8c9d79d6c3e9c28261a734b5a6bd34
ta <git2024ta@gmail.com>
2024-01-29 15:04:42 +0900

    ok

--- src/main/java/jp/ken/rental/controller/SetController.java
+++ src/main/java/jp/ken/rental/controller/SetController.java
@@ -1,61 +1,39 @@
 package jp.ken.rental.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import jp.ken.rental.dao.MembersDao;
-import jp.ken.rental.entity.Members;
-import jp.ken.rental.groups.GroupOrder;
 import jp.ken.rental.model.MemberModel;
 
 
 @Controller
+@RequestMapping("aaa")
+@SessionAttributes({"loginModel", "memberModel"})
 public class SetController {
-	@Autowired
-	private MembersDao membersDao ;
 
-	@RequestMapping(value = "/aaa",method = RequestMethod.GET)
+	@ModelAttribute("memberModel")
+		public MemberModel setupMemberModel() {
+		return new MemberModel();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String toSearch(Model model) {
-		model.addAttribute("membeModel",new MemberModel());
-		model.addAttribute("headline","会員登録");
+
 		return "registration";
 	}
 
-	@RequestMapping(value = "/aaa",method = RequestMethod.POST)
-	public String searchMembers(Model model, @Validated(GroupOrder.class)@ModelAttribute MemberModel memberModel,
-			BindingResult result) {
-		if(result.hasErrors()) {
-		model.addAttribute("headline","会員登録");
+	@RequestMapping(method = RequestMethod.POST)
+	public String searchMembers() {
+
 		return "registration";
 	}
 
-		Members members = new Members();
-		members.setName(memberModel.getName());
-		members.setMail(memberModel.getEmail());
-		members.setPhone(memberModel.getPhone());
-		members.setBirthday(Members.parseDate(memberModel.getBirthday()));
-		members.setCard(memberModel.getCard());
-
-
-		int numberOfRow = membersDao.insert(members);
-		if(numberOfRow == 0) {
-			model.addAttribute("message","登録に失敗しました。");
-			model.addAttribute("headline","会員登録");
-			return "registration";
-		}
-
-		return "redirect:/header";
-    }
-	@RequestMapping(value = "/test",method = RequestMethod.GET)
-	public String toCopm(Model model) {
-		model.addAttribute("headline","会員登録完了");
-
+	@RequestMapping(method = RequestMethod.POST, params = "sss")
+	public String toCopm () {
 
 		return "registrationComplete";
 
